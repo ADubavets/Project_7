94a6ce963bc5d9a058b4da0bbaab913e1fc25edd
ADubavets <d.andrey.f@gmail.com>
2024-08-16 23:55:12 +0300

    16-08-2024-mod

--- project_7/app_1/models.py
+++ project_7/app_1/models.py
@@ -1,2 +1,5 @@
 from django.db import models
 
+
+class Car(models.Model):
+    name = models.CharField('Наименование автомобиля', max_length=30)
---
+++ project_7/app_1/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+def url1(request):
+    return HttpResponse("Ответ 1")
+
+
+def url2(request):
+    return HttpResponse("Ответ 2")
+
+
+def url3(request):
+    return HttpResponse(request)
